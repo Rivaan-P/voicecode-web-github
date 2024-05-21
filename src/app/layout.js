b2c147f1f8cbc1180dfff74e3ba3e2db3b9cbb31
Muchamad Yuda Tri Ananda <69108782+myudak@users.noreply.github.com>
2024-05-22 04:56:46 +0800

    add copy text skeleton

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body class="h-screen" className={inter.className}>
         <Navbar />
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,22 +1,50 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import ReactMarkdown from "react-markdown";
 
+import { toast } from "sonner";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 const ChatMessage = ({ text, sender }) => {
   return (
-    <div
-      className={`chat-message flex items-start gap-2 ${
-        sender === "user" ? "user" : "assistant"
-      }`}
-    >
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt={sender} />
-        <AvatarFallback>A</AvatarFallback>
-      </Avatar>
-      {/* <span className="text-gray-800 text-base leading-relaxed">{text}</span> */}
-      <ReactMarkdown className="text-gray-800 text-base leading-relaxed">
-        {text}
-      </ReactMarkdown>
-    </div>
+    <HoverCard>
+      <HoverCardTrigger
+        onClick={() =>
+          toast("Text Copied!", {
+            description: "not yet implemented :(",
+            action: {
+              label: "close",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+        className="cursor-pointer"
+      >
+        <div
+          className={`chat-message flex items-start gap-2 ${
+            sender === "user" ? "user" : "assistant"
+          }`}
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt={sender} />
+            <AvatarFallback>A</AvatarFallback>
+          </Avatar>
+          {/* <span className="text-gray-800 text-base leading-relaxed">{text}</span> */}
+          <ReactMarkdown className="text-gray-800 text-base leading-relaxed">
+            {text}
+          </ReactMarkdown>
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-auto">
+        <div className="flex justify-between space-x-4">
+          <div>copy</div>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
   );
 };
 
